f6e0026123de0ad896d74e72534bbd92646b3014
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-12-12 19:33:05 +0900

    feat: 회원 정보 조회 기능 구현
    토큰을 통해 조회

--- Backend/src/main/java/com/enjoytrip/member/controller/MemberController.java
+++ Backend/src/main/java/com/enjoytrip/member/controller/MemberController.java
@@ -1,14 +1,19 @@
 package com.enjoytrip.member.controller;
 
+import com.enjoytrip.domain.model.entity.Member;
 import com.enjoytrip.member.dto.MemberCreateDto;
+import com.enjoytrip.member.dto.MemberInfoDto;
 import com.enjoytrip.member.service.MemberService;
 import javax.validation.Valid;
+
+import com.enjoytrip.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -21,5 +26,22 @@ public class MemberController {
     public ResponseEntity<String> joinMember(@Valid @RequestBody MemberCreateDto memberCreateDto) {
         memberService.joinMember(memberCreateDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
+
+    @GetMapping("info")
+    @ResponseStatus(HttpStatus.OK)
+    public MemberInfoDto getMemberInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Authentication: " + authentication);
+        System.out.println("Principal type: " + authentication.getPrincipal().getClass().getName());
+        Long id = userDetails.getId();
+        Member member = memberService.findMemberById(id);
+
+        return MemberInfoDto.builder()
+                .username(member.getUsername())
+                .nickname(member.getNickname())
+                .name(member.getName())
+                .role(member.getRole().name())
+                .build();
     }
 }
