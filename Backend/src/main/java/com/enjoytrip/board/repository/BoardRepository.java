1b373276e0427e5dc7e2c6c86e47e12b085c6470
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-12-13 02:10:11 +0900

    feat: 삭제된 글 조회 불가능

--- Backend/src/main/java/com/enjoytrip/board/repository/BoardRepository.java
+++ Backend/src/main/java/com/enjoytrip/board/repository/BoardRepository.java
@@ -2,8 +2,16 @@ package com.enjoytrip.board.repository;
 
 import com.enjoytrip.domain.model.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query("SELECT b FROM Board b WHERE b.id = :id AND b.isDeleted = false")
+    Optional<Board> findById(@Param("id") Long id);
+
 }
