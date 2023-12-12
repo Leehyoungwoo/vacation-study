f969b3c685b78bd8316f00d4271f986a3dc29565
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-12-13 01:53:27 +0900

    feat: 글 읽기 시 잘못된 게시물 조회를 처리하는 예외 클래스 구현

---
+++ Backend/src/main/java/com/enjoytrip/domain/exception/BoardNotFoundException.java
@@ -0,0 +1,8 @@
+package com.enjoytrip.domain.exception;
+
+public class BoardNotFoundException extends RuntimeException{
+
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+}
