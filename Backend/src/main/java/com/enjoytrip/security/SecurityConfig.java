214428cbdf381d0c874051f05501a09213da6c0e
all-clouds <qkgk61@gmail.com>
2023-12-16 03:17:39 +0900

    학습용으로 주석처리

--- Backend/src/main/java/com/enjoytrip/security/SecurityConfig.java
+++ Backend/src/main/java/com/enjoytrip/security/SecurityConfig.java
@@ -24,25 +24,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
-    private final MemberService memberService;
+//    private final MemberService memberService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        authenticationProvider.setUserDetailsService(memberService);
-        return authenticationProvider;
-    }
+//    @Bean
+//    public AuthenticationProvider daoAuthenticationProvider() {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//        authenticationProvider.setUserDetailsService(memberService);
+//        return authenticationProvider;
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authenticationProvider(daoAuthenticationProvider())
+//                .authenticationProvider(daoAuthenticationProvider())
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
