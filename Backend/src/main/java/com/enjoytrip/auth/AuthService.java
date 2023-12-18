f882dae2fb83cd4f8f25547ff207ff6736836102
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-12-18 14:12:57 +0900

    refactor: autoService 글 쓴 본인과 관리자만 수정 및 삭제할 수 있는 update메서드 구현
    적용은 안되는 걸로 예상

--- Backend/src/main/java/com/enjoytrip/auth/AuthService.java
+++ Backend/src/main/java/com/enjoytrip/auth/AuthService.java
@@ -1,5 +1,10 @@
 package com.enjoytrip.auth;
 
+import com.enjoytrip.board.repository.BoardRepository;
+import com.enjoytrip.domain.exception.BoardNotFoundException;
+import com.enjoytrip.domain.exception.ExceptionMessage;
+import com.enjoytrip.domain.exception.MemberNotFoundException;
+import com.enjoytrip.domain.model.entity.Board;
 import com.enjoytrip.domain.model.entity.Member;
 import com.enjoytrip.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,19 +13,37 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import static com.enjoytrip.domain.exception.ExceptionMessage.BOARD_NOT_FOUND;
+import static com.enjoytrip.domain.exception.ExceptionMessage.MEMBER_NOT_FOUND;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final BoardRepository boardRepository;
 
     public void authenticatePassword(final Long id, final String password) {
         final Member member = memberRepository.findById(id)
-                                              .orElseThrow(() -> new UsernameNotFoundException("회원이 존재하지 않습니다."));
+                                              .orElseThrow(() -> new MemberNotFoundException(MEMBER_NOT_FOUND));
 
         if (!passwordEncoder.matches(password, member.getPassword())) {
             throw new BadCredentialsException("현재 비밀번호가 일치하지 않습니다.");
         }
     }
+
+    public boolean authorizeToUpdateBoard(Long memberId, Long boardId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new BoardNotFoundException(BOARD_NOT_FOUND));
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(MEMBER_NOT_FOUND));
+
+        boolean hasAdminRole = member.getAuthorities().stream()
+                .anyMatch(authority -> "ROLE_ADMIN".equals(authority.getAuthority()));
+
+        return board.getMember().getId().equals(memberId) || hasAdminRole;
+    }
+
 }
