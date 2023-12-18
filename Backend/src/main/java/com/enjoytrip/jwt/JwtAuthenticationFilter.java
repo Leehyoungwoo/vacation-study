ff82c8f2bc9a92991fbcdb5370dced1d804d4b70
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-12-18 15:02:39 +0900

    style: 사용하지 않는 import 제거

--- Backend/src/main/java/com/enjoytrip/jwt/JwtAuthenticationFilter.java
+++ Backend/src/main/java/com/enjoytrip/jwt/JwtAuthenticationFilter.java
@@ -1,17 +1,12 @@
 package com.enjoytrip.jwt;
 
-import com.enjoytrip.domain.model.entity.Member;
-import com.enjoytrip.domain.model.type.Role;
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
