5877a2885c0973a5e98466b62a8c2845c5514c65
all-cloudz <allclouds.lee@gmail.com>
2023-12-20 01:19:09 +0900

    핸들링 하지 못한 예외는 일단 묶어서 한 번에 처리

--- Backend/src/main/java/com/enjoytrip/domain/exception/ExceptionAdvice.java
+++ Backend/src/main/java/com/enjoytrip/domain/exception/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.enjoytrip.domain.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ExceptionAdvice {
 
     @ExceptionHandler({ MethodArgumentNotValidException.class, HttpMessageNotReadableException.class, InvalidEmailFormatException.class, DataIntegrityViolationException.class })
@@ -46,6 +48,7 @@ public class ExceptionAdvice {
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleInternalServerError(Exception e) {
+        log.error("Occurred Error :: ", e);
         return "페이지를 찾을 수 없습니다.";
     }
 }
