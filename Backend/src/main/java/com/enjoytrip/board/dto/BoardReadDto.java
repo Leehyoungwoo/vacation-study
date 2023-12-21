c4c405ab53f3b53928372242cc9a6c42a9eb34bf
LeeHyoungwoo <dlguddn3024@gmail.com>
2023-12-21 14:55:04 +0900

    refactor: 글쓰기 기능 refactoring

--- Backend/src/main/java/com/enjoytrip/board/dto/BoardReadDto.java
+++ Backend/src/main/java/com/enjoytrip/board/dto/BoardReadDto.java
@@ -1,10 +1,13 @@
 package com.enjoytrip.board.dto;
 
+import com.enjoytrip.domain.model.entity.Board;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
@@ -17,4 +20,12 @@ public class BoardReadDto {
     private final Timestamp currentTime;
 
     private final String author;
+
+    public BoardReadDto(Board board) {
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.currentTime = board.getCurrentTime();
+        this.author = board.getMember().getName();
+    }
+
 }
